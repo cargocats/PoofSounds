7ee9e8722a6850ae7a1cc31152444766f071a940
cargocats <ceggy@protonmail.com>
2025-05-19 17:57:52 -0400

    Translatable options and translations for English

--- src/client/java/cargocat/poofsound/config/ModMenuIntegration.java
+++ src/client/java/cargocat/poofsound/config/ModMenuIntegration.java
@@ -1,5 +1,6 @@
 package cargocat.poofsound.config;
 
+import cargocat.poofsound.PoofSound;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import dev.isxander.yacl3.api.*;
@@ -12,23 +13,23 @@ public class ModMenuIntegration implements ModMenuApi {
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return parentScreen -> YetAnotherConfigLib.createBuilder()
                 .save(PoofConfig.HANDLER::save)
-                .title(Text.literal("Poof Sounds"))
+                .title(Text.translatable("poofsounds.name"))
                 .category(ConfigCategory.createBuilder()
-                        .name(Text.literal("Sound Options"))
-                        .tooltip(Text.literal("Control sound settings for poof sounds"))
+                        .name(Text.translatable("poofsounds.sound_options"))
+                        .tooltip(Text.translatable("poofsounds.sound_options.tooltip"))
                         .group(OptionGroup.createBuilder()
-                                .name(Text.literal("Sound"))
-                                .description(OptionDescription.of(Text.literal("Control volume and toggle the mod")))
+                                .name(Text.translatable("poofsounds.group.sound.title"))
+                                .description(OptionDescription.of(Text.translatable("poofsounds.group.sound.description")))
                                 .option(Option.<Boolean>createBuilder()
-                                        .name(Text.literal("Poof Sounds"))
-                                        .description(OptionDescription.of(Text.literal("Are poof sounds enabled?")))
+                                        .name(Text.translatable("poofsounds.name"))
+                                        .description(OptionDescription.of(Text.translatable("poofsounds.option.enabled")))
                                         .binding(true, () -> PoofConfig.HANDLER.instance().poofSoundsEnabled, newVal -> PoofConfig.HANDLER.instance().poofSoundsEnabled = newVal)
                                         .controller(TickBoxControllerBuilder::create)
                                         .build()
                                 )
                                 .option(Option.<Integer>createBuilder()
-                                        .name(Text.literal("Poof Volume"))
-                                        .description(OptionDescription.of(Text.literal("The volume of the poof sound from 0% to 100%")))
+                                        .name(Text.translatable("poofsounds.name.volume"))
+                                        .description(OptionDescription.of(Text.translatable("poofsounds.option.volume")))
                                         .binding(100, () -> PoofConfig.HANDLER.instance().poofVolume, newVal -> PoofConfig.HANDLER.instance().poofVolume = newVal)
                                         .controller(opt -> IntegerSliderControllerBuilder.create(opt)
                                                 .range(0, 200)
