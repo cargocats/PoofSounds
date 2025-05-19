42fd09df899d08caf0a7feb1419b46b42a30be9e
cargocats <ceggy@protonmail.com>
2025-05-19 17:58:44 -0400

    Refactor recentlyDiedEntities

--- src/client/java/cargocat/poofsound/PoofSoundClient.java
+++ src/client/java/cargocat/poofsound/PoofSoundClient.java
@@ -3,43 +3,47 @@ package cargocat.poofsound;
 import cargocat.poofsound.config.PoofConfig;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.sound.PositionedSoundInstance;
+import net.minecraft.client.sound.SoundInstance;
 import net.minecraft.entity.Entity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.math.Vec3d;
 
-import java.util.Iterator;
-import java.util.Map;
-
 public class PoofSoundClient implements ClientModInitializer {
+    public static MinecraftClient minecraftClient = MinecraftClient.getInstance();
     @Override
     public void onInitializeClient() {
         PoofConfig.HANDLER.load();
-        PoofConfig poofConfig = PoofConfig.HANDLER.instance();
+        PoofConfig poofConfigInstance = PoofConfig.HANDLER.instance();
 
         ClientTickEvents.END_CLIENT_TICK.register((client) -> {
-            if (client.world == null) {
+            if (client.world == null || PoofSound.recentlyDiedEntities.isEmpty()) {
                 return;
             }
 
-            Iterator<Map.Entry<Integer, Vec3d>> iterator = PoofSound.recentlyDiedEntities.entrySet().iterator();
-
-            while (iterator.hasNext()) {
-                Map.Entry<Integer, Vec3d> entry = iterator.next();
-                int id = entry.getKey();
-                Entity entity = client.world.getEntityById(id);
+            PoofSound.recentlyDiedEntities.entrySet().removeIf(entry -> {
+                Entity entity = client.world.getEntityById(entry.getKey());
 
                 if (entity != null) {
                     entry.setValue(entity.getPos());
+                    return false;
                 } else {
-                    if (!poofConfig.poofSoundsEnabled) {
-                        return;
+                    if (!poofConfigInstance.poofSoundsEnabled) {
+                        return true;
                     }
-
-                    Vec3d pos = entry.getValue();
-                    client.execute(() -> client.world.playSound(pos.x, pos.y, pos.z, PoofSound.POOF_SOUND_EVENT, SoundCategory.NEUTRAL, PoofConfig.HANDLER.instance().poofVolume / 100.0f, 1.0f, true));
-                    iterator.remove();
+                    Vec3d entityPos = entry.getValue();
+                    client.getSoundManager().play(new PositionedSoundInstance(
+                            PoofSound.POOF_SOUND_EVENT,
+                            SoundCategory.NEUTRAL,
+                            poofConfigInstance.poofVolume / 100.0f,1.0f, SoundInstance.createRandom(),
+                            entityPos.x, entityPos.y, entityPos.z
+                    ));
+                    return true;
                 }
-            }
+            });
         });
+
+        PoofSound.LOGGER.info("Initialized Poof Sounds client!");
     }
 }
